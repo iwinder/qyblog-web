130d826d265f09c74e905a457b5de0333a1aec80
windcoder <windcoderz@foxmail.com>
2020-07-21 22:15:21 +0800

    文章列表组件化

--- qy-web/components/qy-post-list.vue
+++ qy-web/components/qy-post-list.vue
@@ -1,40 +1,45 @@
 <template>
-        <a-list-item slot="renderItem" key="item.title" slot-scope="item">
-                
-                <template   slot="extra" style="width:272px"> 
-                    <template v-if="item.category!=null">
-                        <a href="https://windcoder.com/category/java"  class="category-tag" rel="category tag"> {{item.category.name}}</a>
-                    </template>
-                    <template v-else>                
-                            <a href="https://windcoder.com/category/java"  class="category-tag" rel="category tag"> 未分类</a>   
-                    </template>
-                    <a href="https://windcoder.com/dawenjianfenpianshangchuanjavabanjiandanshixian" class="img-a">
-                                    <img
-                                    width="272px"
-                                    alt="logo"
-                                    :src="item.thumbnail"
-                                /></a>
-                </template>
-                <template v-for="{ type, text } in actions" slot="actions">
-                    <span :key="type">
-                    <a-icon :type="type" style="margin-right: 8px" />
-                    
-                        {{ text }}
-                    </span>
-                </template>
+        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+    <a-list-item slot="renderItem" key="item.title" slot-scope="item">
+        
+        <template   slot="extra" style="width:272px"> 
+            <template v-if="item.category!=null">
+                  <a href="https://windcoder.com/category/java"  class="category-tag" rel="category tag"> {{item.category.name}}</a>
+            </template>
+            <template v-else>                
+                    <a href="https://windcoder.com/category/java"  class="category-tag" rel="category tag"> 未分类</a>   
+            </template>
+            <!-- <a href="https://windcoder.com/dawenjianfenpianshangchuanjavabanjiandanshixian" > -->
+            <nuxt-link  slot="title"  :to="{name:'name',params:{name:item.href, aid:item.id}}" class="img-a">
+                            <img
+                            width="272px"
+                            alt="logo"
+                            :src="item.thumbnail"
+                        />
+                        
+                         </nuxt-link>  
+        </template>
+        <template v-for="{ type, text } in actions" slot="actions">
+            <span :key="type">
+            <a-icon :type="type" style="margin-right: 8px" />
+            
+                {{ text }}
+            </span>
+        </template>
 
-                <a-list-item-meta :description="item.description">
-                    <nuxt-link  slot="title"  :to="{name:'name',params:{name:item.href, aid:item.id}}"> {{ item.title }} </nuxt-link>
-                </a-list-item-meta>
+        <a-list-item-meta :description="item.description">
+            <nuxt-link  slot="title"  :to="{name:'name',params:{name:item.href, aid:item.id}}"> {{ item.title }} </nuxt-link>
+        </a-list-item-meta>
 
-                <a-row style="margin: 5px 0;">
-                        <a-tag color="#f50" v-for=" tag in item.tagStrings" :key="tag">
-                        {{tag}}
-                        </a-tag>
-                    
-                </a-row> 
-                <a-row>   {{ item.content }} </a-row>
-            </a-list-item>
+         <a-row style="margin: 5px 0;">
+                <a-tag color="#f50" v-for=" tag in item.tagStrings" :key="tag">
+                  {{tag}}
+                </a-tag>
+             
+        </a-row> 
+        <a-row class="post-summary">   {{ item.content }}...</a-row>
+    </a-list-item>
+  </a-list>
 </template>
 
 <script >
@@ -44,45 +49,29 @@ export default Vue.extend({
          console.log("listcontext", context);
      },
     props: { 
-            paginationObj: {},
-            listDataObj: {
+            pagination: {},
+            listData: {
                 type: Array,
                 default: []
             },
-            afterSubmit: {
-                type: Function,
-                default: null
-            }, 
-            afterPageChange: {
-                type: Function,
-                default: null
-            }
+            actions: [
+          { type: 'like-o', text: '156' },
+          { type: 'message', text: '2' },
+        ],
+
     },
     watch: {
-        paginationObj(val) {
-            // let _this = this;
-            // _this.pagination = val;
-        },
-        listDataObj(val) {
-            // let _this = this;
-            // _this.listData = val;
-        }
+
     },
     mounted() {
-            let _this = this;
-            _this.pagination = _this.paginationObj;
-             _this.listData = _this.listDataObj;
+
     },
     data() {
         return {
-            listData: [],
-            pagination: {},
-            actions: [
-                    { type: 'like-o', text: '156' },
-                    { type: 'message', text: '2' },
-            ],
+
         }
     },
     
 })
 </script>
+
--- qy-web/pages/page/_id.vue
+++ qy-web/pages/page/_id.vue
@@ -1,44 +1,7 @@
 <template>
     <a-row class="content">
         <a-col :xs="{span:24}"  :lg="{ span: 16}" > 
-            <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-                <a-list-item slot="renderItem" key="item.title" slot-scope="item">
-                    
-                    <template   slot="extra" style="width:272px"> 
-                        <template v-if="item.category!=null">
-                            <a href="https://windcoder.com/category/java"  class="category-tag" rel="category tag"> {{item.category.name}}</a>
-                        </template>
-                        <template v-else>                
-                                <a href="https://windcoder.com/category/java"  class="category-tag" rel="category tag"> 未分类</a>   
-                        </template> 
-                         <nuxt-link  slot="title"  :to="{name:'name',params:{name:item.href, aid:item.id}}" class="img-a">
-                                        <img
-                                        width="272px"
-                                        alt="logo"
-                                        :src="item.thumbnail"
-                                    /> </nuxt-link>
-                    </template>
-                    <template v-for="{ type, text } in actions" slot="actions">
-                        <span :key="type">
-                        <a-icon :type="type" style="margin-right: 8px" />
-                        
-                            {{ text }}
-                        </span>
-                    </template>
-
-                    <a-list-item-meta :description="item.description">
-                        <nuxt-link  slot="title"  :to="{name:'name',params:{name:item.href, aid:item.id}}"> {{ item.title }} </nuxt-link>
-                    </a-list-item-meta>
-
-                    <a-row style="margin: 5px 0;">
-                            <a-tag color="#f50" v-for=" tag in item.tagStrings" :key="tag">
-                            {{tag}}
-                            </a-tag>
-                        
-                    </a-row> 
-                    <a-row>   {{ item.content }}... </a-row>
-                </a-list-item>
-            </a-list>
+           <qy-post-list  :pagination="pagination" :listData="listData"></qy-post-list>
         </a-col>
 
         <a-col :xs="{span:24}"  :lg="{  span: 5, offset: 7 }" > 
@@ -50,7 +13,12 @@
 
 <script  >
 import Vue from 'vue'
+import QyPostList from '~/components/qy-post-list.vue'
+ 
 export default Vue.extend({
+      components: { 
+    QyPostList
+  },
     validate ({ params }) {
         // 必须是number类型
         return /^\d+$/.test(params.id)
@@ -148,8 +116,6 @@ export default Vue.extend({
 })
 </script>
 
-
-<style scoped>
-@import "@/static/css/qy-index.css"
-
+<style lang="scss"   scoped>
+@import "@/assets/css/qy-index.scss"
 </style>
